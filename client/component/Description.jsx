a29cd6db4c76aba187143919423f5f0b3e4a3ae9
yi sun <yisun@yis-MacBook-Pro.local>
2019-07-12 12:02:32 -0700

    clean code in app

--- client/component/Description.jsx
+++ client/component/Description.jsx
@@ -70,8 +70,9 @@ class Description extends React.Component{
         }
     }
     render(){
-        // console.log('props',this.props);
-        const { title, location, host, highlights, desc, detail } = this.props;
+        // console.log('props',this.props.info);
+        const { title, location, host, highlights, desc, detail, amenity } = this.props.info;
+       
         // var title = this.props.title;
         // var location = this.props.location;
         // var host = this.props.host;
@@ -85,7 +86,6 @@ class Description extends React.Component{
         var arrgs= [];
         var bedicon;
         for(var i=1; i<=br; i++){
-            // bedoption = bedoptions[Math.floor(Math.random()*bedoptions.length)]
             var bedoption = beds[i-1];
             if(bedoption==='1 queen bed' || bedoption==='1 king bed'){
                 bedicon = 'm23.96 14.81-2.96-7.41v-5.02a1.39 1.39 0 0 0 -1.39-1.38h-15.22c-.77 0-1.39.62-1.39 1.38v5.02l-2.96 7.41-.04.19v5.61c0 .64.43 1.17 1.01 1.33 0 .02-.01.04-.01.06v1.5a.5.5 0 0 0 1 0v-1.5h20v1.5a.5.5 0 0 0 1 0v-1.5c0-.02-.01-.04-.01-.06a1.39 1.39 0 0 0 1.01-1.33v-5.61zm-19.96-12.43c0-.21.17-.38.39-.38h15.22a.39.39 0 0 1 .39.39v4.61h-1v-1.61c0-.77-.62-1.39-1.39-1.39h-3.21c-.78 0-1.4.62-1.4 1.39v1.61h-2v-1.61c0-.77-.62-1.39-1.39-1.39h-3.22c-.77 0-1.39.62-1.39 1.39v1.61h-1zm14 3.01v3.21a.39.39 0 0 1 -.39.39h-3.21a.39.39 0 0 1 -.4-.38v-3.22a.39.39 0 0 1 .39-.39h3.21a.39.39 0 0 1 .39.39zm-8 0v3.21a.39.39 0 0 1 -.39.4h-3.22a.39.39 0 0 1 -.39-.39v-3.22a.39.39 0 0 1 .39-.39h3.21a.39.39 0 0 1 .39.39zm-6.16 2.61h1.16v.61c0 .77.62 1.39 1.39 1.39h3.21c.78 0 1.4-.62 1.4-1.39v-.61h2v .61c0 .78.62 1.39 1.39 1.39h3.21c.78 0 1.4-.62 1.4-1.39v-.61h1.16l2.8 7h-21.92zm19.16 12.61c0 .21-.18.39-.39.39h-21.22a.39.39 0 0 1 -.39-.39v-4.61h22z'
@@ -117,7 +117,6 @@ class Description extends React.Component{
                     poplistitems.push(
                         <div className='poplistitem' >
                             <div className='categorytitle' key={amencategory[i]}>{amencategory[i]}</div> 
-                            
                                 {subcategory.map((cc,index) =>
                                     <div className='categorycontent' key={cc}>
                                         {cc}  
@@ -236,7 +235,7 @@ class Description extends React.Component{
                 <div className={styles.linemg}>
                     <div className={styles.line}></div>
                 </div>
-                <Amenity amenity={this.props.amenity} showamenitylist={this.showamenitylist.bind(this)} />
+                <Amenity amenity={amenity} showamenitylist={this.showamenitylist.bind(this)} />
                 <div className={styles.linemg}>
                     <div className={styles.line}></div>
                 </div>
