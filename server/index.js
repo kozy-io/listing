ccbcb1a62f86cd1b2d59055c2a927bedb5033cce
yi sun <yisun@yis-MacBook-Pro.local>
2019-07-19 14:50:07 -0700

    compress done

--- server/index.js
+++ server/index.js
@@ -4,17 +4,31 @@ const app = express();
 const bodyParser = require('body-parser');
 const db = require('../database')
 const port = 3000;
+const expressStaticGzip =require("express-static-gzip");
 
 app.use(bodyParser.json());
-app.use('/:listingID',express.static("public"))
+// app.use('/:listingID',express.static("public"));
+app.use('/:listingID', expressStaticGzip('public', {
+    enableBrotli: true,
+    orderPreference: ['br', 'gz'],
+    setHeaders: function (res, path) {
+       res.setHeader("Cache-Control", "public, max-age=31536000");
+    }
+ }));
 
 app.get('/listing/desc/:listingID',(req,res)=>{
     var id = req.params.listingID
     db.findDesc(id,(err,data)=>{
         if(err){
             res.status(500).send(err);
-        }
-        res.json(data[0])
+        } else {
+            console.log(data)
+            if (data.length) {
+                res.json(data[0])
+            } else {
+                res.status(500)
+            }
+        }        
     })
 })
 app.get('/listing/amenity/:listingID',(req,res)=>{
@@ -22,8 +36,13 @@ app.get('/listing/amenity/:listingID',(req,res)=>{
     db.findAmen(id,(err,data)=>{
         if(err){
             res.status(500).send(err);
+        } else {
+            if (data.length) {
+                res.json(data[0])
+            } else {
+                res.status(500)
+            }
         }
-        res.json(data[0])
     })
 })
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,56 @@
-var path = require('path');
-    module.exports = {
-      mode: 'development',
-      watch: true,
-      entry: path.resolve(__dirname, 'client/index.js'),
-      output: {
-        path: path.resolve(__dirname,'public'),
-        filename: 'bundle.js'
-      },
-      module: {
-        rules: [
-          {
-            test: /\.(js|jsx)$/,
-            use: ['babel-loader'],
-            exclude: /node_modules/,
+const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
+
+  module.exports = {
+    mode: 'development',
+    watch: true,
+    entry: path.resolve(__dirname, 'client/index.js'),
+    output: {
+      path: path.resolve(__dirname,'public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          use: ['babel-loader'],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/,
+          use: [{
+            loader: 'style-loader',
           },
           {
-            test: /\.css$/,
-            use: [{
-              loader: 'style-loader',
-            },
-            {
-              loader: 'css-loader',
-              options: {
-                modules: {
-                  localIdentName: '[local]',
-                },
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[local]',
               },
-            }],
-          },
-          
-        ]
-      },
-      resolve: {
-        extensions: ['*','.js','.jsx']
-      }
+            },
+          }],
+        },
+        
+      ]
+    },
+    plugins: [
+      new CompressionPlugin({
+      filename: '[path].gz[query]',
+      algorithm: 'gzip',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.7
+      }),
+      new BrotliPlugin({
+      filename: '[path].br[query]',
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.7
+      })
+    ],
+    resolve: {
+      extensions: ['*','.js','.jsx']
     }
+    
+  }
