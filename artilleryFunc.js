0b2334bf049b6a0e5bd27f19ca3c97a1bb85e2b5
Jonathan Hsieh <jonathan@Jonathans-MacBook-Pro.local>
2019-08-06 11:28:03 -0700

    implement redis for caching

--- artilleryFunc.js
+++ artilleryFunc.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const generateGetRequestData = (userContext, events, done) => {
-  const listingId = Math.floor(Math.random() * 10000000) + 1;
+  const listingId = Math.floor(Math.random() * 100) + 1;
   userContext.vars.listingId = listingId;
   return done();
 }
--- database/index.js
+++ database/index.js
@@ -1,16 +1,45 @@
 const cassandra = require('cassandra-driver');
+const redis = require('./redisConnection');
+const distance = cassandra.types.distance;
 
-const client = new cassandra.Client({contactPoints: ['localhost'], localDataCenter: 'datacenter1', keyspace: 'kozy'});
+const client = new cassandra.Client({
+  contactPoints: ['localhost'],
+  localDataCenter: 'datacenter1',
+  keyspace: 'kozy',
+  pooling: {
+    coreConnectionsPerHost: {
+      [distance.local] : 2,
+      [distance.remote] : 1
+    },
+    maxRequestsPerConnection: 1000
+  }
+});
 
 const getDescription = (id, callback) => {
-  client.execute(`SELECT * FROM description WHERE listingid=${id}`, (err, result) => {
-    if (err){
-      callback(err)
+  let query = `SELECT * FROM description WHERE listingid=${id}`;
+
+  redis.get(query, (err, result) => {
+    if (result) {
+      callback(null, result);
     } else {
-      callback(null, result.rows);
+      client.execute(query, (err, result) => {
+        if (err) {
+          callback(err);
+        } else {
+          redis.setex(query, 1600, JSON.stringify(result.rows));
+          callback(null, result.rows);
+        }
+      });
     }
-    // Run next function in series
   });
+
+  // client.execute(query, (err, result) => {
+  //   if (err) {
+  //     callback(err);
+  //   } else {
+  //     callback(null, result.rows);
+  //   }
+  // });
 }
 
 const getBasicAmenity = (id, callback) => {
